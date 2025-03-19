7b9cd9cb02e90d2dd3ba3210151c853a185de794
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-03-19 09:04:14 +0200

    Write messages

---
+++ src/shared_utils/meesages.py
@@ -0,0 +1,16 @@
+INVALID_TOKEN_MESSAGE = 'Invalid Token'
+EXPIRED_TOKEN_MESSAGE = 'Token has expired'
+INVALID_CREDENTIALS_MESSAGE = 'No user matches the given credentials'
+
+USER_ALREADY_EXIST_MESSAGE = 'User with that email already exists'
+USER_ALREADY_ACTIVE = 'This email is already verified.'
+USER_NOT_FOUND_MESSAGE = 'User not found'
+USER_FORBIDDEN_MESSAGE = 'You are not allowed to perform this action'
+
+TASK_NOT_FOUND_MESSAGE = 'Task not found'
+
+OLLAMA_MODEL_IS_NOT_LOADED = "Ollama is running but the Deep Seek model is not loaded."
+OLLAMA_SERVICE_IS_RUNNING = "Ollama service is running with Deep Seek model loaded"
+OLLAMA_SERVICE_RETURNED_UNEXPECTED_RESPONSE = "Ollama service returned unexpected response"
+CANNOT_CONNECT_TO_OLLAMA_SERVICE = "Cannot connect to Ollama service"
+SEARCH_TASKS_NOT_FOUND_MESSAGE = 'No tasks matching the given search task id'
