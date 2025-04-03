879460fb75640b63123451e8c2058f19c5482308
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-03 11:11:09 +0200

    Remove calls from init files

--- src/shared_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from shared_utils import messages
-from shared_utils.utils import safe_call
-from shared_utils.async_handler import AsyncHandler
--- src/shared_utils/db/__init__.py
+++
@@ -1,2 +0,0 @@
-from shared_utils.db.base import Base
-from shared_utils.db.session import init_db, close_db, get_db
