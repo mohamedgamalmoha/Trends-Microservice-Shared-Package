040cd3080bebdf68c80830639957bd2abadb0797
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-13 13:15:57 +0300

    Remove page number pagination

--- src/shared_utils/pagination/__init__.py
+++ src/shared_utils/pagination/__init__.py
@@ -1,3 +1,1 @@
 from shared_utils.pagination import Paginator
-from shared_utils.pagination.page_number import (PageNumberPaginationQueryParams, PageNumberPaginationResponse,
-                                                 PageNumberPaginator)
